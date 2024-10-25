0d2f8f4a4b71350cfe71dd352559fffff6786cf3
Tw212 <Tewe07001@utb.vaxjo.se>
2024-10-25 13:55:21 +0200

    dawd

--- obj/Debug/net8.0/programering-spel.AssemblyInfo.cs
+++ obj/Debug/net8.0/programering-spel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programering-spel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c2e739276e0cff2948042ce1ac45aa5835fd24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2614475f34b89c3fda8b8966fd0f30a61c2c2b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("programering-spel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programering-spel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
